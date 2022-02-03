9f747faaf2d47c62a41848a7aaccdde124a7c6e3
Wanna101 <78103741+Wanna101@users.noreply.github.com>
2022-02-03 14:34:01 -0700

    Update fzbz.java

--- Challenge3/fzbz.java
+++ Challenge3/fzbz.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 public class fzbz {
-
+	
+	// read in input, the first being fizz, 2nd buzz, etc.
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int fizz = in.nextInt();
 		int buzz = in.nextInt();
 		int range = in.nextInt();
 		
+		// if statement chain if the mods of fizz and buzz == 0 etc.
 		for (int i = 1; i <= range; i++) {
 			if (i % fizz == 0 && i % buzz == 0) {
 				System.out.println("FizzBuzz");
@@ -18,6 +20,8 @@ public class fzbz {
 			else if (i % buzz == 0) {
 				System.out.println("Buzz");
 			} else {
+				// printing range if does not get
+				// Fizz, Buzz, or FizzBuzz
 				System.out.println(i);
 			}
 			
