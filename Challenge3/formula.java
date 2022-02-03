3bcb8c69ee4f6acc207c79affed1d0b1238208a5
Wanna101 <78103741+Wanna101@users.noreply.github.com>
2022-02-03 14:32:19 -0700

    Update formula.java

--- Challenge3/formula.java
+++ Challenge3/formula.java
@@ -7,10 +7,14 @@ public class formula {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// read in input
 		Scanner in = new Scanner(System.in);
 		int numberPoints = in.nextInt();
 		// int number = Integer.parseInt(numberPoints);
 		// System.out.println(number);
+		
+		// measurements to hold last loop's value
+		// for calculation
 		double last_one = -100000;
 		double last_two = 0;
 		double total = 0;
@@ -24,7 +28,7 @@ public class formula {
 				last_two = two; 							
 				continue;
 			}
-			
+			// calculate trapezoid
 			last_two = (last_two + two) / 2;
 			last_one = (one - last_one);
 			double calculation = last_one * last_two;
