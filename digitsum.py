d0d352cdc8b586000cdda07fd2fb7c25dddd3a79
Wanna101 <78103741+Wanna101@users.noreply.github.com>
2022-03-30 23:38:02 -0600

    Add files via upload

---
+++ digitsum.py
@@ -0,0 +1,16 @@
+
+def digit(n):
+    if n == 0:
+        return 0
+    return (n % 10 + + digit(int(n / 10)))
+
+
+number = int(input())
+for i in range(number):
+    first, second = input().split(' ')
+    total = 0
+    for j in range(int(first), int(second) + 1):
+        total += digit(j)
+    print(total)
+    
+    
